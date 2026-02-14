ab60c772e3262123d1cac0f42c6aa58ff3a4bb1f
jaimetur <jaimetur@gmail.com>
2026-02-14 04:22:00 +0100

    Some refactoring and User Guide Automations

--- tools/Build-Binary.py
+++ tools/Build-Binary.py
@@ -471,14 +471,14 @@ def main(compiler='pyinstaller', compile_in_one_file=COMPILE_IN_ONE_FILE):
     # Paths for CHANGELOG.md, RELEASE-NOTES.md and DOWNLOAD.md
     download_filepath = os.path.join(root_dir, 'DOWNLOAD.md')
     changelog_filepath = os.path.join(root_dir, 'CHANGELOG.md')
-    current_release_filepath = os.path.join(current_dir, 'RELEASE-NOTES.md')
+    current_release_filepath = os.path.join(root_dir, 'RELEASE-NOTES.md')
 
     # Extract the body of the current release from CHANGELOG.md
     _extract_release_body(input_file=changelog_filepath, output_file=current_release_filepath, download_file=download_filepath)
     print(f"File '{current_release_filepath}' created successfully!.")
 
     # Save build_info.txt
-    with open(os.path.join(current_dir, 'build_info.txt'), 'w') as file:
+    with open(os.path.join(root_dir, 'build_info.txt'), 'w') as file:
         file.write('OPERATING_SYSTEM=' + OPERATING_SYSTEM + '\n')
         file.write('ARCHITECTURE=' + ARCHITECTURE + '\n')
         file.write('TOOL_NAME=' + TOOL_NAME + '\n')
@@ -545,7 +545,7 @@ def compile(compiler='pyinstaller', compile_in_one_file=COMPILE_IN_ONE_FILE):
         script_compiled_with_version_os_arch_extension = f"{TOOL_NAME_WITH_VERSION_OS_ARCH}.run"
 
     # Append additional info into build_info.txt
-    with open(os.path.join(current_dir, 'build_info.txt'), 'a') as file:
+    with open(os.path.join(root_dir, 'build_info.txt'), 'a') as file:
         file.write('COMPILER=' + str(compiler) + '\n')
         file.write('SCRIPT_COMPILED=' + os.path.abspath(script_compiled_with_version_os_arch_extension) + '\n')
         print('')
